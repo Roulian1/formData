0e9449d97acfc46154ee357163ba7fc1b6a230bd
Julien Elinckx <julienelinckx@hotmail.com>
2022-08-31 13:20:20 +0200

    modif fd

---
+++ script.js
@@ -0,0 +1,42 @@
+// FormData object
+//DOMContentLoaded: content loaded when the web page is open
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let fd = new FormData();
+    let current = new Date();
+    fd.append('name', 'BeCode');
+    fd.append('name', 'Xavier');
+    fd.append('ID', 3333);
+    fd.append('date_now', current.toLocaleString());
+
+    console.log(Array.from(fd));
+
+    document.querySelector('#form pre').textContent = JSON.stringify(Array.from(fd), '/t', 2);
+
+// boucle for
+
+    // for (let obj of fd) {
+    //     console.log(obj);
+    // }
+
+// entries iteration
+
+    // for (let pair of fd.entries()) {
+    //     console.log(pair[0]+ ', '+ pair[1]);
+    //  }
+
+// delete an obj
+
+    // fd.delete('ID');
+
+// get 
+
+    // console.log(fd.get('name'));
+
+// set
+
+    // fd.set('taille', 46);
+    // console.log(fd.get('taille'));
+
+})
+
